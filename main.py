091a9f1da385325a76abef0d8c4a5fc1b91a1829
Aman Virmani <aman.virmani26@gmail.com>
2020-05-07 05:19:58 -0400

    updated main file to show demo

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from utils import planner, obstacle_space
+import cv2
 
 
 if __name__ == "__main__":
-    pass
+    grid = obstacle_space.Map()
+
+    planner = planner.PathPlanning(grid, (0, 0), (9, 6))
+    planner.explore()
+    cv2.destroyAllWindows()
