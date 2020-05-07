3679a676300d6dfd4a801e7038c9ebbea0bef2c0
umang <umang.rastogi96@gmail.com>
2020-05-06 20:10:13 -0400

    [obstacle_space.py] Implement methods to move obstacles

--- utils/obstacle_space.py
+++ utils/obstacle_space.py
@@ -1,7 +1,7 @@
 # Import necessary standard libraries
 import cv2
 import numpy as np
-from random import choice
+from random import randint
 # Import necessary constants
 from utils.constants import *
 
@@ -25,7 +25,7 @@ class Map:
                                        dtype=np.int8)
         self.scaled_centers = SCALING_FACTOR * self.circle_centers.copy()
         # Define empty world and add obstacles to it
-        self.map_img = self.generate_check_image()
+        self.map_img = self.generate_map()
         # Get image to search for obstacles
         self.check_img = self.draw_obstacles()
 
@@ -46,7 +46,7 @@ class Map:
             # Draw the circle
             cv2.circle(img, (int(center[0]), int(center[1])), self.circle_radius + thresh, self.black, -1)
 
-    def generate_check_image(self):
+    def generate_map(self):
         """
         Get eroded image to check for obstacles considering the robot radius and clearance
         :return: image with obstacle space expanded to distance threshold between robot and obstacle
@@ -82,12 +82,42 @@ class Map:
         Method to move obstacles around on the map
         :return: map-image with updated positions of the obstacles
         """
-        pass
+        for i in range(len(self.circle_centers)):
+            center = self.circle_centers[i][0], self.circle_centers[i][1]
+            self.map_img[center[1]][center[0]] = 0
+            action = randint(0, TOTAL_ACTIONS)
+            self.circle_centers[i] = self.take_action(action, center)
+            self.map_img[self.circle_centers[i][1]][self.circle_centers[i][0]] = -1
+        return self.map_img
 
-    def take_action(self, center_obstacle):
+    @staticmethod
+    def take_action(action, center_obstacle):
         """
         Method to change centers of the obstacles
+        :param action: an integer indicating the action
         :param center_obstacle: a tuple containing center of the obstacle
         :return: a tuple containing new centers of the obstacle
         """
-        pass
+        center_new = center_obstacle
+        # Take required and get new center coordinates of the obstacles
+        if action == ACTION_SPACE['up']:
+            center_new = center_obstacle[0], center_obstacle[1] - VELOCITY_OBSTACLE
+        elif action == ACTION_SPACE['down']:
+            center_new = center_obstacle[0], center_obstacle[1] + VELOCITY_OBSTACLE
+        elif action == ACTION_SPACE['left']:
+            center_new = center_obstacle[0] - VELOCITY_OBSTACLE, center_obstacle[1]
+        elif action == ACTION_SPACE['right']:
+            center_new = center_obstacle[0] + VELOCITY_OBSTACLE, center_obstacle[1]
+        elif action == ACTION_SPACE['top_left']:
+            center_new = center_obstacle[0] - VELOCITY_OBSTACLE, center_obstacle[1] - VELOCITY_OBSTACLE
+        elif action == ACTION_SPACE['top_right']:
+            center_new = center_obstacle[0] + VELOCITY_OBSTACLE, center_obstacle[1] - VELOCITY_OBSTACLE
+        elif action == ACTION_SPACE['bottom_left']:
+            center_new = center_obstacle[0] - VELOCITY_OBSTACLE, center_obstacle[1] + VELOCITY_OBSTACLE
+        elif action == ACTION_SPACE['bottom_right']:
+            center_new = center_obstacle[0] + VELOCITY_OBSTACLE, center_obstacle[1] + VELOCITY_OBSTACLE
+        # Check for out of bounds condition
+        if ((center_new[0] <= 0 or center_new[0] > MAP_SIZE[0] - 1) or
+                (center_new[1] <= 0 or center_new[1] > MAP_SIZE[1] - 1)):
+            center_new = center_obstacle
+        return center_new
