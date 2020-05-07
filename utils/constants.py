b5b19bc382ddfc9844ee98fe77517a9cd0fc55c4
umang <umang.rastogi96@gmail.com>
2020-05-06 20:09:20 -0400

    [constants.py] Add constant to action space of obstacles

--- utils/constants.py
+++ utils/constants.py
@@ -4,3 +4,7 @@ MAP_SIZE = 10, 10
 SCALING_FACTOR = 50
 # Define velocity of the obstacle
 VELOCITY_OBSTACLE = 1   # unit/time-step
+# Define action space for the obstacle
+TOTAL_ACTIONS = 8
+ACTION_SPACE = {'up': 0, 'down': 1, 'left': 2, 'right': 3,
+                'top_left': 4, 'top_right': 5, 'bottom_left': 6, 'bottom_right': 7}
