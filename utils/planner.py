dfeca2f39540ae180081b728a2f689cd5863b00b
Aman Virmani <aman.virmani26@gmail.com>
2020-05-08 19:46:23 -0400

    added code for generating and saving waypoints

--- utils/planner.py
+++ utils/planner.py
@@ -17,6 +17,7 @@ class PathPlanning:
                                                  int(constants.SCALING_FACTOR*(self.target[0]+0.5))),
                    self.grid_obj.circle_radius, constants.TARGET_BGR, -1)
         self.distance[self.target[0], self.target[1]] = 0
+        self.action_encoding = {'00': None, '0-1': 0, '1-1': 1, '10': 2, '11': 3, '01': 4, '-11': 5, '-10': 6, '-1-1': 7}
         # Define video-writer of open-cv to record the exploration and final path
         video_format = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
         self.video_output = cv2.VideoWriter('video_output.avi', video_format, 200.0,
@@ -34,12 +35,14 @@ class PathPlanning:
                                                  int(constants.SCALING_FACTOR*(self.robot[0]+0.5))),
                    self.grid_obj.circle_radius, constants.FREE_SPACE_BGR, -1)
         min_dist_loc = self.get_next_move()
+        action = np.subtract(min_dist_loc, self.robot)
         if 0 <= min_dist_loc[0] < self.grid.shape[0] and 0 <= min_dist_loc[1] < self.grid.shape[1]:
             self.robot = min_dist_loc
         self.grid[self.robot[0]][self.robot[1]] = constants.ROBOT_LOC_VALUE
         cv2.circle(self.grid_obj.animation_img, (int(constants.SCALING_FACTOR*(self.robot[1]+0.5)),
                                                  int(constants.SCALING_FACTOR*(self.robot[0]+0.5))),
                    self.grid_obj.circle_radius, constants.ROBOT_BGR, -1)
+        return self.action_encoding[str(action[0])+str(action[1])]
 
     def get_next_move(self):
         min_dist = constants.MAX_DISTANCE
@@ -71,10 +74,11 @@ class PathPlanning:
     def explore(self):
         # 2 - target, 1- empty, 0 - obstacles, 3- robot
         step = 0
+        waypoints = []
         while True:
             step += 1
-            self.grid_obj.map_img = self.grid_obj.update_obstacle_space()
-            self.grid = np.copy(self.grid_obj.map_img)
+            #self.grid_obj.map_img = self.grid_obj.update_obstacle_space()
+            #self.grid = np.copy(self.grid_obj.map_img)
             old_distance = np.copy(self.distance)
             for row in range(len(self.grid)):
                 for col in range(len(self.grid[0])):
@@ -86,14 +90,16 @@ class PathPlanning:
                         f_value = self.get_f_value((row, col))
                         old_distance[row][col] = min([constants.MAX_DISTANCE, f_value])
             self.distance = old_distance
-            self.move_robot()
+            action = self.move_robot()
+            waypoints.append([self.robot, action])
             self.grid_obj.map_img = np.copy(self.grid)
             for _ in range(250):
                 self.video_output.write(self.grid_obj.animation_img)
-            # cv2.imshow('map', self.grid_obj.animation_img)
-            # cv2.waitKey(250)
+            cv2.imshow('map', self.grid_obj.animation_img)
+            cv2.waitKey(250)
             if self.robot == self.target:
                 print('Reached Target!!...')
                 self.video_output.release()
+                np.save('waypoints.npy', waypoints)
                 # cv2.waitKey(0)
                 break
